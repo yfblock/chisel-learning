98e6119697012809707aa0bdc95ea934a1dbde5e
yfblock <321353225@qq.com>
2025-08-27 04:22:30 +0800

    feat: add build.sh and linker.lds to remove useless sections

--- c/build.sh
+++ c/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-riscv64-linux-gnu-gcc -static -nostartfiles -ffreestanding -O2 -nostdlib main.c
+riscv64-linux-gnu-gcc \
+  -static -nostartfiles \
+  -ffreestanding -O2 -nostdlib \
+  -T linker.lds \
+  -march=rv64i -mabi=lp64 \
+  main.c
 rust-objcopy --binary-architecture=riscv64 a.out --strip-all -O binary a.bin
 # convert binary to hexfile, 4bytes per-line
 xxd -p -c 4 a.bin | sed 's/../& /g'  > a.hex
